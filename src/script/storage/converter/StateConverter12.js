ed9582155c9599ee969712f2c7d52486a841d2b7
Denis Peters <denis.peters@udo.edu>
2020-12-30 01:11:18 +0100

    fix converter12;

--- src/script/storage/converter/StateConverter12.js
+++ src/script/storage/converter/StateConverter12.js
@@ -33,22 +33,29 @@ StateConverter.register(function(state) {
         }
     }
     const extra_shops = {};
-    for (const [key, value] of Object.entries(state.extra.shops_items)) {
-        for (let i = 0; i < 8; ++i) {
-            extra_shops[`${key}/${i}.item`] = value[i].item;
-            extra_shops[`${key}/${i}.price`] = value[i].price;
+    if (state.extra.shops_items != null) {
+        for (const [key, value] of Object.entries(state.extra.shops_items)) {
+            for (let i = 0; i < 8; ++i) {
+                extra_shops[`${key}/${i}.item`] = value[i].item;
+                extra_shops[`${key}/${i}.price`] = value[i].price;
+            }
         }
     }
-    for (const [key, value] of Object.entries(state.extra.shops_bought)) {
-        for (let i = 0; i < 8; ++i) {
-            extra_shops[`${key}/${i}.bought`] = !!value[i];
+    if (state.extra.shops_bought != null) {
+        for (const [key, value] of Object.entries(state.extra.shops_bought)) {
+            for (let i = 0; i < 8; ++i) {
+                extra_shops[`${key}/${i}.bought`] = !!value[i];
+            }
         }
     }
-    for (const [key, value] of Object.entries(state.extra.shops_names)) {
-        for (let i = 0; i < 8; ++i) {
-            extra_shops[`${key}/${i}.name`] = value[i];
+    if (state.extra.shops_names != null) {
+        for (const [key, value] of Object.entries(state.extra.shops_names)) {
+            for (let i = 0; i < 8; ++i) {
+                extra_shops[`${key}/${i}.name`] = value[i];
+            }
         }
     }
     // collect data
+    res.extra.shops = extra_shops;
     return res;
 });
