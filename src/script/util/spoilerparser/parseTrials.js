804ba5b4058134ed9d14e455f0f87a1cf9b19195
ZidArgs <zidargs@gmail.com>
2021-12-07 20:58:33 +0100

    enhance error handling for spoiler parser;

--- src/script/util/spoilerparser/parseTrials.js
+++ src/script/util/spoilerparser/parseTrials.js
@@ -1,18 +1,16 @@
-import spoilerErrorAlert from "./spoilerErrorAlert.js";
-
-export default function parseTrials(target = {}, data = {}, trans = {}) {
+export default function parseTrials(errorDialogHandler, target = {}, data = {}, trans = {}) {
     const trial_trans = trans["trials"];
 
     for (const i in data) {
         const v = data[i];
         if (trial_trans[i] != null) {
             if (Array.isArray(i)) {
-                console.warn("Unexpected Array within active trials")
-                spoilerErrorAlert.prepareAlert();
+                console.warn("Unexpected Array within active trials");
+                errorDialogHandler.add("Unexpected Array within active trials");
             } else {
                 if (trial_trans[i]["values"][v] === undefined) {
-                    console.warn("[" + i + ": " + v + "] is a invalid Trials value.")
-                    spoilerErrorAlert.prepareAlert();
+                    console.warn("[" + i + ": " + v + "] is a invalid Trials value.");
+                    errorDialogHandler.add("[" + i + ": " + v + "] is a invalid Trials value.");
                 } else {
                     target[trial_trans[i]["name"]] = trial_trans[i]["values"][v];
                 }
