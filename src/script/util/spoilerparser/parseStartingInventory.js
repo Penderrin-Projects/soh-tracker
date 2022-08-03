47769e5811b8c25e94c1161f4c924b2c5aea5834
Denis Peters <zidargs@gmail.com>
2022-08-03 23:26:39 +0200

    fix spoiler parsing for progressive items;

--- src/script/util/spoilerparser/parseStartingInventory.js
+++ src/script/util/spoilerparser/parseStartingInventory.js
@@ -11,11 +11,48 @@ function addValue(target, key, value) {
 export default function parseStartingInventory(addError, target = {}, settingsSpoiler = {}, trans = {}) {
     target.startItems = target.startItems ?? {};
 
-    for (const key of INVENTORY_KEYS) {
-        const data = settingsSpoiler[key] ?? {};
-        const starting_trans = trans[key] ?? {};
-        if (Array.isArray(data)) {
-            // OLD RANDO SPOILER FOR STARTITEMS
+    if (typeof settingsSpoiler["starting_items"] === "object" && !Array.isArray(settingsSpoiler["starting_items"])) {
+        // NEW RANDO SPOILER FOR STARTITEMS
+        const data = settingsSpoiler["starting_items"] ?? {};
+        const starting_trans = trans["starting_items_new"] ?? {};
+        for (const item in data) {
+            const spoilerValue = parseInt(data[item]);
+            if (typeof item != "string") {
+                addError(`Unexpected type "${typeof item}" within starting items`);
+            } else {
+                const transData = starting_trans[item];
+                if (transData == null) {
+                    addError(`Unknown Starting item "${item}"`);
+                } else if (transData === false) {
+                    // ignore them
+                } else if (typeof transData == "string") {
+                    if (!isNaN(spoilerValue)) {
+                        addValue(target.startItems, transData, spoilerValue);
+                    } else {
+                        addValue(target.startItems, transData, 1);
+                    }
+                } else {
+                    const name = transData["name"];
+                    if (typeof name != "string") {
+                        addError(`Translation for Starting item "${item}" is errornous`);
+                    } else if (!isNaN(spoilerValue)) {
+                        addValue(target.startItems, name, spoilerValue);
+                    } else {
+                        const transValue = parseInt(transData["value"]);
+                        if (!isNaN(transValue)) {
+                            addValue(target.startItems, name, transValue);
+                        } else {
+                            addValue(target.startItems, name, 1);
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        // OLD RANDO SPOILER FOR STARTITEMS
+        for (const key of INVENTORY_KEYS) {
+            const data = settingsSpoiler[key] ?? {};
+            const starting_trans = trans[key] ?? {};
             for (const item of data) {
                 if (typeof item != "string") {
                     addError(`Unexpected type "${typeof item}" within starting items`);
@@ -35,37 +72,6 @@ export default function parseStartingInventory(addError, target = {}, settingsSp
                                 addValue(target.startItems, name, value);
                             } else {
                                 addValue(target.startItems, name, 1);
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            // NEW RANDO SPOILER FOR STARTITEMS
-            for (const item in data) {
-                if (typeof item != "string") {
-                    addError(`Unexpected type "${typeof item}" within starting items`);
-                } else {
-                    const transData = starting_trans[item];
-                    if (transData == null) {
-                        addError(`Unknown Starting item "${item}" for "${key}"`);
-                    } else if (typeof transData == "string") {
-                        addValue(target.startItems, transData, 1);
-                    } else {
-                        const name = transData["name"];
-                        if (typeof name != "string") {
-                            addError(`Translation for Starting item "${item}" in "${key}" is errornous`);
-                        } else {
-                            const spoilerValue = parseInt(data[item]);
-                            if (!isNaN(spoilerValue)) {
-                                addValue(target.startItems, name, spoilerValue);
-                            } else {
-                                const transValue = parseInt(transData["value"]);
-                                if (!isNaN(transValue)) {
-                                    addValue(target.startItems, name, transValue);
-                                } else {
-                                    addValue(target.startItems, name, 1);
-                                }
                             }
                         }
                     }
