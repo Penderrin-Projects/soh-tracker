3ce7c347aa5ab7507c679197397e667da8bb2331
Denis Weiß <zidargs@gmail.com>
2024-07-05 20:18:28 +0200

    fix ap mq dungeons sync;

--- src/script/util/archipelago/APSettingsTranslator.js
+++ src/script/util/archipelago/APSettingsTranslator.js
@@ -2,11 +2,6 @@ import OptionsTransAPResource from "../../resource/OptionsTransAPResource.js";
 
 const trans = OptionsTransAPResource.get();
 
-const BLACKLISTED_OPTIONS = [
-    "collectible_override_flags",
-    "collectible_flag_offsets"
-];
-
 export function translateAPSettings(data = {}) {
     const target = {
         areaActiveLists: {},
@@ -18,51 +13,88 @@ export function translateAPSettings(data = {}) {
     const dungeons = trans["dungeons"];
     const tricks = trans["tricks"];
 
-    for (const [key, value] of Object.entries(data)) {
+    const {
+        spawn_positions,
+        logic_tricks,
+        dungeon_shortcuts,
+        dungeon_shortcuts_list,
+        mq_dungeons_mode,
+        mq_dungeons_list,
+        mq_dungeons_count,
+        ...rest_data
+    } = data;
+
+    // spawn positions
+    target.options["shuffle_spawn_child"] = !!(spawn_positions & 1);
+    target.options["shuffle_spawn_adult"] = !!(spawn_positions >> 1 & 1);
+
+    // tricks
+    for (const [apTrick, trick] of Object.entries(tricks)) {
+        target.options[trick] = logic_tricks.includes(apTrick);
+    }
+
+    // dungeon shortcuts
+    if (dungeon_shortcuts === 1) {
+        for (const dungeonName in dungeons) {
+            const internalName = dungeons[dungeonName];
+            target.options[`dungeon_shortcuts.${internalName}`] = dungeon_shortcuts_list.includes(dungeonName);
+        }
+    } else if (dungeon_shortcuts === 2) {
+        for (const dungeonName in dungeons) {
+            const internalName = dungeons[dungeonName];
+            target.options[`dungeon_shortcuts.${internalName}`] = true;
+        }
+    } else {
+        for (const dungeonName in dungeons) {
+            const internalName = dungeons[dungeonName];
+            target.options[`dungeon_shortcuts.${internalName}`] = false;
+        }
+    }
+
+    // mq dungeons
+    if (mq_dungeons_mode === 0 || (mq_dungeons_mode === 3 && mq_dungeons_count === 0)) {
+        for (const dungeonName in dungeons) {
+            const internalName = dungeons[dungeonName];
+            target.areaActiveLists[internalName] = "v";
+        }
+    } else if (mq_dungeons_mode === 1 || (mq_dungeons_mode === 3 && mq_dungeons_count === 12)) {
+        for (const dungeonName in dungeons) {
+            const internalName = dungeons[dungeonName];
+            target.areaActiveLists[internalName] = "mq";
+        }
+    } else if (mq_dungeons_mode === 2) {
+        for (const dungeonName in dungeons) {
+            const internalName = dungeons[dungeonName];
+            target.areaActiveLists[internalName] = mq_dungeons_list.includes(dungeonName) ? "mq" : "v";
+        }
+    } else {
+        for (const dungeonName in dungeons) {
+            const internalName = dungeons[dungeonName];
+            target.areaActiveLists[internalName] = "";
+        }
+    }
+
+    for (const [key, value] of Object.entries(rest_data)) {
         try {
-            if (BLACKLISTED_OPTIONS.includes(key)) {
+            const transData = settingsTrans[key];
+            if (transData == null) {
+                errors.push(`no valid translation found for option "${key}"`);
+                continue;
+            } else if (transData === false) {
                 // ignore key
                 continue;
-            }
-            if (key === "mq_dungeons_list") {
-                for (const dungeonName in dungeons) {
-                    const internalName = dungeons[dungeonName];
-                    target.areaActiveLists[internalName] = value.includes(dungeonName) ? "mq" : "v";
-                }
-            } else if (key === "dungeon_shortcuts_list") {
-                for (const dungeonName in dungeons) {
-                    const internalName = dungeons[dungeonName];
-                    target.options[`dungeon_shortcuts.${internalName}`] = value.includes(dungeonName);
-                }
-            } else if (key === "spawn_positions") {
-                target.options["shuffle_spawn_child"] = !!(value & 1);
-                target.options["shuffle_spawn_adult"] = !!(value >> 1 & 1);
-            } else if (key === "logic_tricks") {
-                for (const [apTrick, trick] of Object.entries(tricks)) {
-                    target.options[trick] = value.includes(apTrick);
-                }
-                continue;
             } else {
-                const transData = settingsTrans[key];
-                if (transData == null) {
-                    errors.push(`no valid translation found for option "${key}"`);
-                    continue;
-                } else if (transData === false) {
-                    // ignore key
-                    continue;
+                const parsedValue = parseInt(value) || 0;
+                if (typeof transData === "string") {
+                    target.options[transData] = parsedValue;
                 } else {
-                    const parsedValue = parseInt(value) || 0;
-                    if (typeof transData === "string") {
-                        target.options[transData] = parsedValue;
-                    } else {
-                        const {name, values, overwrite = false} = transData;
-                        const transValue = values[parsedValue];
-                        if (transValue == null) {
-                            // ignore value
-                            continue;
-                        } else if (overwrite || target.options[name] == null) {
-                            target.options[name] = transValue;
-                        }
+                    const {name, values, overwrite = false} = transData;
+                    const transValue = values[parsedValue];
+                    if (transValue == null) {
+                        // ignore value
+                        continue;
+                    } else if (overwrite || target.options[name] == null) {
+                        target.options[name] = transValue;
                     }
                 }
             }
