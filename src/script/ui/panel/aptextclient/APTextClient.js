7f092fd5b5b4e15e89a923c726b1d32c00e113b9
Denis Weiß <zidargs@gmail.com>
2024-09-05 23:59:56 +0200

    add basic structure of APTextClient;

--- src/script/ui/panel/aptextclient/APTextClient.js
+++ src/script/ui/panel/aptextclient/APTextClient.js
@@ -1,5 +1,35 @@
+import Panel from "/emcJS/ui/layout/Panel.js";
+
+import TPL from "./APTextClient.js.html" assert {type: "html"};
+import STYLE from "./APTextClient.js.css" assert {type: "css"};
+
 // TODO
 // text client with filter options (command list cache)
 // show hints as sortable filterable grid
 // add hint item function through clicking the desired icons (generate text user needs to send)
 // add hint location function through clicking the desired locations (generate text user needs to send)
+
+class APTextClient extends Panel {
+
+    #apTabsEl;
+
+    constructor() {
+        super();
+        this.shadowRoot.append(TPL.generate());
+        STYLE.apply(this.shadowRoot);
+        /* --- */
+        this.#apTabsEl = this.shadowRoot.getElementById("ap-tabs");
+    }
+
+    switchToApLog() {
+        this.#apTabsEl.active = "ap-log";
+    }
+
+    switchToHints() {
+        this.#apTabsEl.active = "hints";
+    }
+
+}
+
+Panel.registerReference("ap-text-client", APTextClient);
+customElements.define("ootrt-aptextclient", APTextClient);
