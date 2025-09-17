0e3e1806ef8fa0ecb93ca2420db97c7053287437
Denis Weiß <zidargs@gmail.com>
2025-09-17 10:14:27 +0200

    fix checksum calculation for archive creation;

--- src/script/StateRecovery.js
+++ src/script/StateRecovery.js
@@ -97,7 +97,7 @@ const createArchive = (() => {
     const VERSION_MIN = 20;
     const ATTR_FILE = 0x0020;
     async function createLocalFileEntry(file, fileHeaderOffset) {
-        const rawData = await file.arrayBuffer();
+        const fileContents = await file.text();
         const rawStream = file.stream();
         const compressionStream = new CompressionStream("deflate-raw");
         const compressedStream = rawStream.pipeThrough(compressionStream);
@@ -109,9 +109,9 @@ const createArchive = (() => {
             compressionMethod: 0x08,
             lastModifiedTime: timeToDOS(lastModified),
             lastModifiedDate: dateToDOS(lastModified),
-            crc: crc32(rawData),
+            crc: crc32(fileContents),
             compressedSize: compressedData.byteLength,
-            uncompressedSize: rawData.byteLength,
+            uncompressedSize: fileContents.length,
             fileNameLength: file.name.length,
             extraLength: 0,
             fileName: file.name,
